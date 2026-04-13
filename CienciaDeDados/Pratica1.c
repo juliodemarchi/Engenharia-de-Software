4100b84069a5f9c942b5a412963539b642418259
Julio Demarchi <129809896+juliodemarchi@users.noreply.github.com>
2026-04-13 17:28:42 -0400

    Create Pratica1.c

---
+++ CienciaDeDados/Pratica1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+     char nome[30];
+     int idade:
+} Pessoa;
+
+#define MAX 5
+typedef struct {
+Pessoa itens[MAX];
+int inicio;
+int fim;
+int total;
+} Fila;
+
+int main() {
+  return 0;
+}
